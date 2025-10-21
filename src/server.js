c248fa05ccfd63fbfb045e456415ed5d294bb982
yasin <125811137+yasinbesni@users.noreply.github.com>
2025-10-21 10:02:16 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ export const setupServer = () => {
   app.use(express.json()); // JSON body parse için mutlaka ekle
 
   // Rotalar buraya eklenecek
-  app.use('/cotacts', contactsRoutes);
+  app.use('/api/contacts', contactsRoutes);
 
   // 404 için fallback
  app.use((req, res) => {
