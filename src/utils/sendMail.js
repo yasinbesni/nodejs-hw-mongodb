d208081c02ab672916cf76706d93f407007eb9d9
Yasin Besni <125811137+yasinbesni@users.noreply.github.com>
2025-11-11 00:23:23 +0300

    updated

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -6,6 +6,7 @@ import { getEnvVar } from '../utils/getEnvVar.js';
 const transporter = nodemailer.createTransport({
   host: getEnvVar(SMTP.SMTP_HOST),
   port: Number(getEnvVar(SMTP.SMTP_PORT)),
+    secure: false,
   auth: {
     user: getEnvVar(SMTP.SMTP_USER),
     pass: getEnvVar(SMTP.SMTP_PASSWORD),
