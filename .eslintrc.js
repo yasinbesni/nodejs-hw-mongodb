788711ed49511a50f252aae7ca73bcec0c3903ca
Yasin Besni <125811137+yasinbesni@users.noreply.github.com>
2025-11-10 19:41:19 +0300

    The codes in the branch were deleted and rewritten

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: { node: true, es2021: true },
-  extends: ['eslint:recommended'],
-  parserOptions: { ecmaVersion: 12 },
-  rules: { 'no-unused-vars': 'warn' },
-};
--- src/utils/jwt.js
+++
@@ -1,8 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const { JWT_SECRET } = process.env;
-
-export const generateToken = (payload, expiresIn = "5m") =>
-  jwt.sign(payload, JWT_SECRET, { expiresIn });
-
-export const verifyToken = (token) => jwt.verify(token, JWT_SECRET);
