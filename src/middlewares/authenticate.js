4fca054d9b2bebaa3a6e2e63f6f8738a3c0e0e7b
Yasin Besni <125811137+yasinbesni@users.noreply.github.com>
2025-11-11 15:52:36 +0300

    testing

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,6 +1,6 @@
 import createHttpError from 'http-errors';
 
-import { SessionsCollection } from '../db/models/session.js';
+import { SessionsCollection } from '../db/models/temp.js';
 import { UsersCollection } from '../db/models/user.js';
 
 export const authenticate = async (req, res, next) => {
